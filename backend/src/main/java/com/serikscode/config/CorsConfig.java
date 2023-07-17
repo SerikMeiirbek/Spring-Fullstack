dcd64147547b5fb59b8ff6fddf2f247af86cc138
serik <serik.meiirbek@gmail.com>
2023-07-17 18:37:30 -0400

    Updated CorsConfig.java source.registerCorsConfiguration("/**", configuration);

--- backend/src/main/java/com/serikscode/config/CorsConfig.java
+++ backend/src/main/java/com/serikscode/config/CorsConfig.java
@@ -33,7 +33,9 @@ public class CorsConfig {
         configuration.setAllowedHeaders(allowedHeaders);
         configuration.setExposedHeaders(expectedHeaders);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", configuration);
+//        source.registerCorsConfiguration("/api/**", configuration);
+        source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 }
