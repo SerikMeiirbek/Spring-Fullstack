0e9d17f227614ed8e96f32e5d306fc238bf65d1e
serik <serik.meiirbek@gmail.com>
2023-07-18 13:11:22 -0400

    Added SecurityFilterChainConfig.java, JWTUtl.java

---
+++ backend/src/main/java/com/serikscode/jwt/JWTUtil.java
@@ -0,0 +1,52 @@
+package com.serikscode.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JWTUtil {
+
+    private static final String secretKey =
+            "foobar_123456789foobar_123456789foobar_123456789foobar_123456789";
+
+    public String issueToken(String subject){
+        return issueToken(subject, Map.of());
+    }
+
+    public String issueToken(String subject, String ... scopes){
+        return issueToken(subject, Map.of("scopes", scopes));
+    }
+
+    public String issueToken(
+            String subject,
+            Map<String, Object> claims
+    ) {
+        String token = Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuer("https://customers.com")
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(
+                        Date.from(
+                                Instant.now().plus(15, ChronoUnit.DAYS)
+                        )
+                )
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+        return token;
+    }
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+}
