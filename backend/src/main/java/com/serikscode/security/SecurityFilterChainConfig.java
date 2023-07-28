0b077f2b8c2a03dc8045edb0dbce02a86f1f6322
serik <serik.meiirbek@gmail.com>
2023-07-27 21:28:35 -0400

    Expose health info metrix and add application-dev.yml

--- backend/src/main/java/com/serikscode/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/serikscode/security/SecurityFilterChainConfig.java
@@ -44,6 +44,12 @@ public class SecurityFilterChainConfig {
                          "/ping"
                  )
                  .permitAll()
+                 .requestMatchers(
+                         HttpMethod.GET,
+                         "/actuator/**"
+
+                 )
+                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
